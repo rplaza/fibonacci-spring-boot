91a51a24a039d00b66b3c87a47de3c200aa6b7ba
Raul Plaza <rplaza@MacBook-Pro-de-Raul.local>
2020-10-10 17:46:00 +0200

    Added docker set up and some sentences to README.md

--- README.md
+++ README.md
@@ -10,3 +10,25 @@ If you ask for a sequence of 7 elements you will receive this one:
 `[ 0, 1, 1, 2, 3, 5, 8]`
 
 For the specifications of the API REST you could access to `http://localhost:8081/swagger-ui.html`.
+
+This application could executed in two ways:
+
+1 - As a spring boot application in console:
+
+    - Download the application from https://github.com/rplaza/fibonacci-spring-boot.git
+    - Compile the code: `mvn clean install`
+    - Run the application: `mvn spring-boot:run`
+    
+2 - This application create a docker container when it is compiling, so you can execute it as a docker application:
+    
+    - Download the application from https://github.com/rplaza/fibonacci-spring-boot.git
+    - Compile the code: `mvn clean install`
+    - Run the application: `docker run -p 8081:8081 rplaza/fibonacci-spring-boot:1.0.0-SNAPSHOT`   
+    
+NOTE
+----
+This application works under some environment needs:
+ - Maven 2 or upper
+ - Java 8
+ - Docker installed locally
+     
--- src/main/java/com/inditex/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/inditex/fibonacci/controller/FibonacciController.java
@@ -1,6 +1,7 @@
 package com.inditex.fibonacci.controller;
 
 import com.inditex.fibonacci.domain.FibonacciResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.inditex.fibonacci.service.Fibonacci;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
-public class FibonacciController {
+public class FibonacciController implements ErrorController {
+
+    private final static String ERROR_PATH = "/error";
 
     @Autowired
     private Fibonacci fibonacci;
@@ -36,4 +44,26 @@ public class FibonacciController {
         }
         return response;
     }
+
+    @RequestMapping(value = ERROR_PATH, method = RequestMethod.GET)
+    @ResponseBody
+    public String errorHtml(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        return String.format("<html>" +
+                        "<body>" +
+                        "<h2>What's the hell are you doing here?</h2>" +
+                        "<h3>You make a request to the wrong way. Please take a look at the documentation: http://localhost:8081/swagger-ui.html</h3>" +
+                        "<div>Status code: <b>%s</b></div>" +
+                        "<div>Exception Message: <b>%s</b></div>" +
+                        "<body>" +
+                        "</html>",
+                statusCode, exception==null? "N/A": exception.getMessage());
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
 }
